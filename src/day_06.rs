deaac04bc5e77fd52a63886884f626265c62e360
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2024-12-06 12:18:23 -0500

    Unify day 6 part 1 and 2

--- src/day_06.rs
+++ src/day_06.rs
@@ -28,7 +28,7 @@ fn parse_input(input: String) -> Vec<Vec<char>> {
     input.lines().map(|line| line.chars().collect()).collect()
 }
 
-fn transform_input_part_two(input: Vec<Vec<char>>) -> Vec<Vec<Position>> {
+fn transform_input(input: Vec<Vec<char>>) -> Vec<Vec<Position>> {
     input
         .iter()
         .map(|row| {
@@ -65,31 +65,8 @@ fn find_initial_position(input: &[Vec<char>]) -> Position {
     pos
 }
 
-fn step_part_one(input: &mut Vec<Vec<char>>, current_pos: &mut Position) -> Option<()> {
-    // Returns Some(()) if we successfully took a step
-    // Returns None if we exited the map
-    let next_pos = Position {
-        x: current_pos.x + current_pos.dir.1,
-        y: current_pos.y + current_pos.dir.0,
-        dir: current_pos.dir,
-    };
-    let next_char = input.get(next_pos.y as usize)?.get(next_pos.x as usize)?;
-    match next_char {
-        '#' => {
-            current_pos.rotate();
-            step_part_one(input, current_pos)
-        }
-        _ => {
-            input[current_pos.y as usize][current_pos.x as usize] = 'X';
-            input[next_pos.y as usize][next_pos.x as usize] = '^';
-            *current_pos = next_pos;
-            Some(())
-        }
-    }
-}
-
-fn step_part_two(input: &mut Vec<Vec<Position>>, current_pos: &mut Position) -> Option<bool> {
-    // Returns Some(true) if we have been in this position before
+fn step(input: &mut Vec<Vec<Position>>, current_pos: &mut Position) -> Option<bool> {
+    // Returns Some(true) if we have been in this position before (loop detected)
     // Returns Some(false) if we haven't been in this position before and we succesfully took a step
     // Returns None if we exited the map
 
@@ -104,7 +81,7 @@ fn step_part_two(input: &mut Vec<Vec<Position>>, current_pos: &mut Position) -> 
     let next_location = input.get(next_pos.y as usize)?.get(next_pos.x as usize)?;
     if next_location.dir == OBSTRUCTION {
         current_pos.rotate();
-        step_part_two(input, current_pos)
+        step(input, current_pos)
     } else {
         input[current_pos.y as usize][current_pos.x as usize] = *current_pos;
         *current_pos = next_pos;
@@ -112,34 +89,41 @@ fn step_part_two(input: &mut Vec<Vec<Position>>, current_pos: &mut Position) -> 
     }
 }
 
-fn walk_part_two(simulation: &mut Vec<Vec<Position>>, starting_pos: Position) -> bool {
+fn walk(simulation: &mut Vec<Vec<Position>>, starting_pos: Position) -> bool {
     let mut current_pos = starting_pos;
-    while let Some(is_loop) = step_part_two(simulation, &mut current_pos) {
+    while let Some(is_loop) = step(simulation, &mut current_pos) {
         if is_loop {
             return true;
         }
     }
+    // Mark our position one last time, since we exited step() without having done so
     simulation[current_pos.y as usize][current_pos.x as usize] = current_pos;
     false
 }
 
 pub fn part_one(input: String) -> u32 {
-    let mut input = parse_input(input);
-    let mut current_pos = find_initial_position(&input);
-    while step_part_one(&mut input, &mut current_pos).is_some() {}
+    let input = parse_input(input);
+    let starting_pos = find_initial_position(&input);
+    let mut input = transform_input(input);
+    walk(&mut input, starting_pos);
     input
         .iter()
-        .map(|row| row.iter().filter(|c| **c == 'X').count())
+        .map(|row| {
+            row.iter()
+                .filter(|c| [UP, DOWN, LEFT, RIGHT].contains(&c.dir))
+                .count()
+        })
         .sum::<usize>() as u32
-        + 1 // Add one for the last step before exiting
 }
 
 pub fn part_two(input: String) -> u32 {
     let input = parse_input(input);
     let starting_pos = find_initial_position(&input);
-    let input = transform_input_part_two(input.clone());
+    let input = transform_input(input);
+    // Walk the path once first so we can identify all the locations where we need to place an obstruction
+    // Placing an obstruction not on the path will never re-route the path, so don't need to evaluate those cases
     let mut unobstructed_path = input.clone();
-    walk_part_two(&mut unobstructed_path, starting_pos);
+    walk(&mut unobstructed_path, starting_pos);
     let mut answer = 0;
     for row_idx in 0..input.len() {
         for col_idx in 0..input[row_idx].len() {
@@ -148,7 +132,7 @@ pub fn part_two(input: String) -> u32 {
             {
                 let mut simulation = input.clone();
                 simulation[row_idx][col_idx].dir = OBSTRUCTION;
-                if walk_part_two(&mut simulation, starting_pos) {
+                if walk(&mut simulation, starting_pos) {
                     answer += 1;
                 }
             }
